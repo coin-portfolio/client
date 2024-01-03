dbad6bfb3147528a1b6926eb9f0baae2c029c299
hyemin065 <p62517@gmail.com>
2024-01-03 16:43:43 +0900

    feat: 이메일 확인 timer hook 작업, timer값 분,초로 변경하는 utils함수 추가

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,8 +1,14 @@
 export const isValidValue = (value: string, label: string, regEx: any, regExText: string) => {
-  console.log(value);
   if (value.trim() === '') {
     return `${label}를 입력해주세요`;
   } else if (!regEx.test(value)) {
     return regExText;
   }
+};
+
+export const formatSeconds = (time: number) => {
+  const minutes = String(Math.floor(time / 60)).padStart(2, '0');
+  const seconds = String(time % 60).padStart(2, '0');
+
+  return { minutes, seconds };
 };
